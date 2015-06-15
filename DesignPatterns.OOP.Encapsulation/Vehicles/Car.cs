f86bb5f2cd6c571f6175cc157c32b4b8cf2e102d
Rafael de Oliveira Benetti <rafaeldeoliveirabenetti@gmail.com>
2015-06-14 21:27:31 -0300

    Add encapsulation projects.

---
+++ DesignPatterns.OOP.Encapsulation/Vehicles/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.OOP.Encapsulation.Vehicles
+{
+    public class Car
+    {
+        public Car()
+        {
+            this._maximumSpeed = 280;
+            this._numberOfWheels = 4;
+        }
+
+        //Encapsuling car's details.
+        //Composition, objects in another objects.
+        public Engine _engine;
+        private double _maximumSpeed { get; set; }
+        private int _numberOfWheels { get; set; }
+
+        public int GetNumberOfWheels()
+        {
+            return _numberOfWheels;        
+        }
+
+        public double GetMaximumSpeed()
+        {
+            return _maximumSpeed;        
+        }
+    }
+}
---
+++ DesignPatterns.OOP.Encapsulation/Vehicles/Engine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.OOP.Encapsulation.Vehicles
+{
+    public class Engine
+    {
+        public int Potency { get; set; }
+        public int Power { get; set; }
+    }
+}
