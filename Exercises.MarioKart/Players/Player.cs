0b270931add11b5e83183eeb3aa980d5a51ea256
Rafael de Oliveira Benetti <rafaelbenetti@cwi.com.br>
2017-02-02 19:53:33 -0200

    test(refactor): add cs project of test

--- Exercises.MarioKart/Players/Player.cs
+++ Exercises.MarioKart/Players/Player.cs
@@ -9,7 +9,19 @@ namespace Exercises.MarioKart.Players
     {
         public Player(string name)
         {
+            if (string.IsNullOrEmpty(name))
+                throw new InvalidNameException("The player's name is required.");
+
             this.Name = name;
+        }
+
+        public Player(string name, PlayerLevelEnum level)
+        {
+            if (string.IsNullOrEmpty(name))
+                throw new InvalidNameException("The player's name is required.");
+
+            this.Name = name;
+            this.Level = level;
         }
 
         public string Name { get; private set; }
